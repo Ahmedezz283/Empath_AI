ff017062d88bce8a824ce20bd312a63df861b6b8
Mido Azoz <ahmadezz2013@gmail.com>
2025-10-12 19:11:53 +0300

    test

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Empath_AI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Empath_AI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("ConnectionString"),
+        sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,              // Try up to 5 times
+                maxRetryDelay: TimeSpan.FromSeconds(10),  // Wait up to 10 sec between retries
+                errorNumbersToAdd: null        // You can specify SQL error codes if needed
+            );
+        }));
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
