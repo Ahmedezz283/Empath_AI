149473a375701a333f9e82c034904a5f5f1cbc82
Mido Azoz <ahmadezz2013@gmail.com>
2025-10-17 23:14:17 +0300

    test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(options =>
+/*builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("ConnectionString"),
         sqlServerOptionsAction: sqlOptions =>
@@ -16,12 +16,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
                 maxRetryDelay: TimeSpan.FromSeconds(10),  // Wait up to 10 sec between retries
                 errorNumbersToAdd: null        // You can specify SQL error codes if needed
             );
-        }));
+        }));*/
 
 builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+/*builder.Services.AddAuthentication("Cookies").AddCookie("Cookies", options =>
+{
+    options.LoginPath = "/Account/login";
+    options.AccessDeniedPath = "/";
+});*/
 
 var app = builder.Build();
 
@@ -35,6 +40,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
