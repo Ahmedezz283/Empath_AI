1231a68d8ed681d5d6007dc0a6b749280a896dec
amany <amanyabdelnaby1569@gmail.com>
2025-10-26 00:34:52 +0300

    dto

---
+++ DTO/ConversationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Empath_AI.DTO
+{
+    public class ConversationDto
+    {
+
+        public int Conversations_ID { get; set; }
+        public int User_ID { get; set; }
+        public string Title { get; set; }
+
+
+    }
+}
